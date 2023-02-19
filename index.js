916d7affe304879fc1d13913126d555102acceda
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-02-19 22:08:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce(function (acc, val) {
+      return acc + val;
+    }, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    return (
+      this.sides[0] === this.sides[1] &&
+      this.sides[1] === this.sides[2] &&
+      this.sides[0] === this.sides[2]
+    );
+  }
+
+  get num() {
+    return this.sides;
+  }
+}
+
+class Square extends Polygon{
+    get isValid() {
+        return (
+          this.sides[0] === this.sides[1] &&
+          this.sides[1] === this.sides[2] &&
+          this.sides[0] === this.sides[2] &&
+          this.sides[3] === this.sides[1]
+        );
+      }
+    get area(){
+        return this.sides[0] ** 2
+    }  
+}
